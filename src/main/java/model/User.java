3d27f7358afbbde06fd76a254a0c330655d7e1d5
PhanVanManh <imseirousmanh@gmail.com>
2026-03-20 20:51:41 +0700

    refactor(model): use Data anno from lombok

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,7 +1,12 @@
 package model;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
+@Builder
 public class User {
     private Integer id;
     private String username;
@@ -9,64 +14,4 @@ public class User {
     private String fullName;
     private LocalDateTime createdAt;
 
-    public User() {
-    }
-
-    public User(Integer id, String username, String passwordHash, String fullName, LocalDateTime createdAt) {
-        this.id = id;
-        this.username = username;
-        this.passwordHash = passwordHash;
-        this.fullName = fullName;
-        this.createdAt = createdAt;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                       "id=" + id +
-                       ", username='" + username + '\'' +
-                       ", fullName='" + fullName + '\'' +
-                       ", createdAt=" + createdAt +
-                       '}';
-    }
 }
