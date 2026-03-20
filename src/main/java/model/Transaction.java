d484f9356cfc569c81eaf0224272dc526b62186e
PhanVanManh <imseirousmanh@gmail.com>
2026-03-20 21:35:29 +0700

    refactor(model): use enum for type and status of transaction

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -2,22 +2,26 @@ package model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import util.constant.TransactionStatus;
+import util.constant.TransactionType;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@Getter
+@Setter
 public class Transaction {
     private String id; // UUID
     private Integer senderAccountId;
     private Integer receiverAccountId;
     private BigDecimal amount;
     private BigDecimal fee;
-    private String type; // TRANSFER, DEPOSIT, WITHDRAW
-    private String status; // SUCCESS, FAILED
+    private TransactionType type; // TRANSFER, DEPOSIT, WITHDRAW
+    private TransactionStatus status; // SUCCESS, FAILED
     private String message;
     private LocalDateTime createdAt;
-
-
 }
